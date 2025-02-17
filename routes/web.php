53e5a3ddfe915ad927be9df8c7c4ff59f274ad70
B00177535 Nora Klarin <B00177535@myTUDublin.ie>
2025-02-17 15:07:27 +0000

    step 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
+Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
